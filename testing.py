0a5ec81f538b7d0b049fed38dcad4316e6443e81
datlv-carmd <datlv@carmd.com>
2026-03-29 16:25:15 +0700

    two GPUs usage

--- testing.py
+++ testing.py
@@ -25,40 +25,48 @@ from losses import *
 from data import *
 from utils.utils import create_path_models
 from utils.test_utils import *
-from utils.device import get_device, is_cuda
+from utils.device import get_device, get_map_location, is_cuda
 from ptflops import get_model_complexity_info
 
-# Set CUDA device only if available
+# Set CUDA devices - use all available GPUs
 if is_cuda():
-    os.environ["CUDA_VISIBLE_DEVICES"]= "0"
+    num_gpus = torch.cuda.device_count()
+    os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(str(i) for i in range(num_gpus))
 
 #parameters for saving model
 PATH_MODEL= create_path_models(opt['save'])
 
 
-def load_model(model, path_weights):
+def load_model(model, path_weights, rank=0, use_multi=False):
 
-    map_location = 'cpu'
+    map_location = get_map_location(rank)
     checkpoints = torch.load(path_weights, map_location=map_location, weights_only=False)
     weights = checkpoints['params']
 
-    macs, params = get_model_complexity_info(model, (3, 256, 256), print_per_layer_stat=False, verbose=False)
-    print('Network complexity: ' ,macs, params)
+    if rank == 0:
+        macs, params = get_model_complexity_info(model, (3, 256, 256), print_per_layer_stat=False, verbose=False)
+        print('Network complexity: ' ,macs, params)
+
+    # Add 'module.' prefix when loading into DDP-wrapped model
+    if use_multi:
+        weights = {'module.' + key: value for key, value in weights.items()}
 
     model.load_state_dict(weights)
-    print('Loaded weights correctly')
+    if rank == 0:
+        print('Loaded weights correctly')
 
     return model
 
 def run_evaluation(rank, world_size):
 
+    use_multi = is_cuda() and world_size > 1
     setup(rank, world_size=world_size)
     # LOAD THE DATALOADERS
     test_loader, _ = create_test_data(rank, world_size=world_size, opt = opt['datasets'])
     # DEFINE NETWORK
-    model, _, _ = create_model(opt['network'], rank=rank)
+    model, _, _ = create_model(opt['network'], rank=rank, use_multi=use_multi)
 
-    model = load_model(model, opt['save']['path'])
+    model = load_model(model, opt['save']['path'], rank=rank, use_multi=use_multi)
     metrics_eval = {}
 
     # Ensure all processes have reached this point (only for DDP)
@@ -80,9 +88,10 @@ def run_evaluation(rank, world_size):
     cleanup()
 
 def main():
-    world_size = 1
     if is_cuda():
-        mp.spawn(run_evaluation, args =(world_size,), nprocs=world_size, join=True)
+        world_size = torch.cuda.device_count()
+        print(f'Running evaluation on {world_size} GPU(s)')
+        mp.spawn(run_evaluation, args=(world_size,), nprocs=world_size, join=True)
     else:
         print(f'Running evaluation on: {get_device()}')
         run_evaluation(rank=0, world_size=1)
